8d6761649558c1eb36d4649a8c734ce2db149d12
victor acosta <progamacion1698@gmail.com>
2022-09-21 22:30:50 -0300

    first commit

---
+++ src/datos.js
@@ -0,0 +1,42 @@
+    
+    // const losDatos =(info)=>{
+    //     let todaLaInfo = {}
+    //     fetch('https://mario-kart-tour-api.herokuapp.com/api/v1/drivers', {
+    //         method: "GET",
+    //         headers: {"Content-type": "application/json;charset=UTF-8"}
+    //     })
+    //      .then(response => response.json()) 
+    //      .then(json =>{
+    //         todaLaInfo = json
+    //         console.log(todaLaInfo)
+
+    //         todaLaInfo.forEach(conductor => {
+    //             conductor = todaLaInfo
+    //             console.log(conductor)
+    //         });
+    //     });
+        
+    // }
+    // // export default losDatos
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
